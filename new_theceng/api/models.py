a2fa5222f03f16cff2d3cf6a4483d01fc507d2a7
kgarayev <kenangarayev@gmail.com>
2023-11-14 21:18:48 +0000

    change

--- new_theceng/api/models.py
+++ new_theceng/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=50, default="", unique=True)
+    password = models.CharField(max_length=50, default="")
+    name = models.CharField(max_length=50, default="")
+    surname = models.CharField(max_length=50, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
